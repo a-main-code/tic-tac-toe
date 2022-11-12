28fd2bbafd59c98244b796f216fffcc139e6607d
a-main-code <117528878+a-main-code@users.noreply.github.com>
2022-11-12 16:03:02 -0300

    feat(match): selecting first player

--- Assets/_Project/GameMatch/Scripts/MatchManager.cs
+++ Assets/_Project/GameMatch/Scripts/MatchManager.cs
@@ -10,10 +10,12 @@ namespace GameMatch
     {
         [SerializeField] private GridSize _gridSize;
         [SerializeField] private GridArea _grid;
+        [SerializeField] private PlayerType _currentPlayer;
 
         private void Awake()
         {
             BuildGrid();
+            SelectFirstPlayer();
         }
 
         private void BuildGrid()
@@ -23,7 +25,16 @@ namespace GameMatch
 
         private void OnSlotClicked(GridSlot slot)
         {
+            // TODO: Regras quando é clicado no slot
+        }
+
+        private void SelectFirstPlayer()
+        {
+            bool isXPlayer = UnityEngine.Random.Range(0, 2) == 0;
             
+            _currentPlayer = isXPlayer ?
+                PlayerType.X :
+                PlayerType.O;
         }
     }
 }
